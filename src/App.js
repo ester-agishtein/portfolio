f67bf983ad5bb684578ef010de565f6115593bdb
ester-agishtein <eagishte@student.touro.edu>
2020-01-19 00:07:51 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import RecycleInfo from "./Info-Recycle";
+import LamaInfo from "./LamaInfo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Col from "react-bootstrap/Col";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <Route path="/Poke-A-Lama-Info">
+            <LamaInfo />
+          </Route>
+
+          <Route path="/React-Set-Recycle-Info">
+            <RecycleInfo />
+          </Route>
+          <Route exact path="/">
+            <Col bsPrefix="componentCol">
+              <Col className="info">
+                <h1>Ester S. Agishtein</h1>
+                <p>
+                  I am senior C.S student at Touro college and am a MediaTech
+                  intern at NBCUniversal. I currently work primarily with React
+                  and am interested in front-end development and web
+                  technologies. I am fascinated by the media industry, and am
+                  passionate about the development of technology which
+                  contributes to storytelling and human connection.
+                </p>
+              </Col>
+
+              <Col className="projects">
+                <h1>My projects</h1>
+
+                <ul>
+                  <li>
+                    <Link to="/" style={{ color: "gold" }}>
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/React-Set-Recycle-Info"
+                      style={{ color: "gold" }}
+                    >
+                      React, Set, Recycle!
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/Poke-A-Lama-Info" style={{ color: "gold" }}>
+                      Poke A Lama
+                    </Link>
+                  </li>
+                </ul>
+              </Col>
+            </Col>
+          </Route>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
