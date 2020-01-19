9ee0b4ec6c9efced75507591e80aa71dce6853c4
ester-agishtein <eagishte@student.touro.edu>
2020-01-19 02:13:53 -0500

    app

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <h1>I'm breathing!!</h1>
         <BrowserRouter>
           <Route path={process.env.PUBLIC_URL + "/Poke-A-Lama-Info"}>
             <LamaInfo />
