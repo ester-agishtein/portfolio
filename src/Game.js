eb82276571b06ed7a110c6e4505c1036e1b8ccdf
ester-agishtein <eagishte@student.touro.edu>
2020-01-28 20:09:13 -0500

    Game sizing mobile

--- src/Game.js
+++ src/Game.js
@@ -197,7 +197,7 @@ export default class Game extends Component {
               src={process.env.PUBLIC_URL + "/paperBin.png"}
               x={window.innerWidth - 250}
               y={window.innerHeight - 400}
-              width={window.innerWidth - window.innerWidth / 20}
+              width={window.innerWidth - window.innerWidth / 5}
               height={window.innerHeight / 3}
             />
             <ObjectImage
