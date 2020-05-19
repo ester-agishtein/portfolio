0d0fad61ee8deeac1fc6d185667612f686affd4c
ester-agishtein <eagishte@student.touro.edu>
2020-05-18 20:11:48 -0400

    Updates

---
+++ precache-manifest.496df1fbf9edf543c7303974c44fda58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868295cf1fb4b83d12667e6ecfef4c81",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "074a4a0649981ff0f2f7",
+    "url": "/portfolio/static/css/main.fc600352.chunk.css"
+  },
+  {
+    "revision": "61dc99c475ec7dfbfbe9",
+    "url": "/portfolio/static/js/2.ce0719cf.chunk.js"
+  },
+  {
+    "revision": "3ceb2b6a201a53ffca76af49eafd1617",
+    "url": "/portfolio/static/js/2.ce0719cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "074a4a0649981ff0f2f7",
+    "url": "/portfolio/static/js/main.a6e2c454.chunk.js"
+  },
+  {
+    "revision": "917ec6e8909d7acd6ee7",
+    "url": "/portfolio/static/js/runtime-main.a42e6d7c.js"
+  },
+  {
+    "revision": "35189145109f6c440ccb5ee0338a2f76",
+    "url": "/portfolio/static/media/backimg.35189145.jpg"
+  }
+]);
