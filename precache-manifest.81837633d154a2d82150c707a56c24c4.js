a7875a6fa204be038055848eaab71960d29c586b
ester-agishtein <eagishte@student.touro.edu>
2020-05-18 21:59:14 -0400

    Updates

--- precache-manifest.81837633d154a2d82150c707a56c24c4.js
+++ precache-manifest.81837633d154a2d82150c707a56c24c4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8eb4cc18406590dae5d18b608b5cd6fd",
+    "revision": "99b6ae03797d5891177269158822534a",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "28de233e50a5eb4e1867",
+    "revision": "592573e8b28d276ec419",
     "url": "/portfolio/static/css/main.fc600352.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.ce0719cf.chunk.js.LICENSE"
   },
   {
-    "revision": "28de233e50a5eb4e1867",
-    "url": "/portfolio/static/js/main.047e1d75.chunk.js"
+    "revision": "592573e8b28d276ec419",
+    "url": "/portfolio/static/js/main.cef6df76.chunk.js"
   },
   {
     "revision": "917ec6e8909d7acd6ee7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e1c3361cf66fffd4ae96da09d6c7869a.js"
+  "/portfolio/precache-manifest.81837633d154a2d82150c707a56c24c4.js"
 );
 
 self.addEventListener('message', (event) => {
