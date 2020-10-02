76e446921f2f732aba78a6d3a3bb1d6067008289
Esty <eagishte@student.touro.edu>
2020-10-02 01:32:47 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a8ea6c2802fc23b3c2232b7644195ea4.js"
+  "/portfolio/precache-manifest.03f86ea82d0dd26763897d8e569aac5e.js"
 );
 
 self.addEventListener('message', (event) => {
