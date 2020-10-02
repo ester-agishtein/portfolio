0cefbd8fa4c2b20457609f0d6fef2e898c79cf56
Esty <eagishte@student.touro.edu>
2020-10-02 01:35:26 -0400

    Updates

---
+++ precache-manifest.d2b56202211d9557f2c5f277e4c2f6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1154938700ff2efb08e5c7f26eee9219",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88465d94f10f8d81aeb8",
+    "url": "./static/css/main.94813c25.chunk.css"
+  },
+  {
+    "revision": "2e43066a97296bcd7daa",
+    "url": "./static/js/2.286d449a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.286d449a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88465d94f10f8d81aeb8",
+    "url": "./static/js/main.927c88d6.chunk.js"
+  },
+  {
+    "revision": "065a2b5eb77bc5db245d",
+    "url": "./static/js/runtime-main.19b0f8a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.03f86ea82d0dd26763897d8e569aac5e.js"
+  "./precache-manifest.d2b56202211d9557f2c5f277e4c2f6e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
